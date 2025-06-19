b68730a4e6ceedce8ee24af3ddf61e76f975fb94
AmeyaMercury <158019769+AmeyaMercury@users.noreply.github.com>
2025-06-19 13:29:12 +0530

    Update env.js

--- appointment_api/config/env.js
+++ appointment_api/config/env.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 4000,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/medical_appointmentmongodb+srv://abcd:abcd@cluster0.e2kwbac.mongodb.net/',
+  MONGODB_URI: process.env.MONGODB_URI || '..........',
   JWT_SECRET: process.env.JWT_SECRET || 'fallback_secret',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '7d',
   NODE_ENV: process.env.NODE_ENV || 'development'
